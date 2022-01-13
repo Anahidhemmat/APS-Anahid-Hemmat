66d616d7145434fd2be73facc3b8de49f110bd11
Anahid <anahidhemmat2130@gmail.com>
2022-01-13 12:41:51 +0400

    objects

--- folder-3/practice.js
+++ folder-3/practice.js
@@ -11,3 +11,14 @@ function sortArray(array) {
   return Array.from(array).sort();
 }
 console.log(sortArray(arr));
+
+//Given an object of the stolen items and a limit, return the difference between the total value of those items and the limit of the policy.
+
+function calculateDifference(obj, limit) {
+  let sum = 0;
+  for (let keys in obj) {
+    sum += obj[keys];
+  }
+  return sum - limit;
+}
+console.log(calculateDifference({ skate: 10, painting: 20 }, 19));
