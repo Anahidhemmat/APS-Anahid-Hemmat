a22e572b17057736f26694a5f4a921adc1136cc5
Anahid <anahidhemmat2130@gmail.com>
2022-01-15 19:23:09 +0400

    quiz

--- folder-5/jobrani.js
+++ folder-5/jobrani.js
@@ -7,8 +7,31 @@ let roll = `you rolled a ${dice1} and a ${dice2}. they sum ${dice1 + dice2}`;
 console.log(roll);
 
 //prompt
-let input = promp("How old are you?");
-let age = parseInt(input);
-let birthday = 1400 - age;
+// let input = prompt("How old are you?");
+// let age = parseInt(input);
+// let birthday = 1400 - age;
 
-console.log(birthday);
+// console.log(birthday);
+
+//if statment
+
+let number = 40;
+if (number % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+//Quiz traffic signal
+
+let trafficlight = "stop";
+
+if (trafficlight === "stop") {
+  console.log("red");
+} else if (trafficlight === "slow") {
+  console.log("yellow");
+} else if (trafficlight === "go") {
+  console.log("green");
+} else {
+  console.log("purpulr");
+}
