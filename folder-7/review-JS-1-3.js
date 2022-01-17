5460fc20cb9b8464a4bd3efca3f4cb53208942ac
Anahid <anahidhemmat2130@gmail.com>
2022-01-17 19:36:02 +0400

    review

--- folder-7/review-JS-1-3.js
+++ folder-7/review-JS-1-3.js
@@ -136,3 +136,24 @@ let numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 let multiply = numbers.map((number) => number * 100);
 
 console.log(multiply); //[ 10, 20, 30, 40, 50 ]
+
+/* 
+  Using .forEach() print the numbers 1 to 15, with some exceptions:
+  - For multiples of 3 print “Fizz” instead of the number 
+  - For the multiples of 5 print “Buzz”. 
+  - For numbers which are multiples of both 3 and 5 print “FizzBuzz”
+
+  An array with numbers 1-15 has been provided.
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function newArray(number) {
+  if (number % 3 === 0) return "Fizz";
+  if (number % 5 === 0) return "Buzz";
+  if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
+
+  return number;
+}
+
+arr.map(newArray).forEach((element) => console.log(element));
