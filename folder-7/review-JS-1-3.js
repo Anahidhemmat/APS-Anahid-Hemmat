757e4eaf8feebfe1af2a58d7a06b973201459473
Anahid <anahidhemmat2130@gmail.com>
2022-01-17 17:37:36 +0400

    find method

--- folder-7/review-JS-1-3.js
+++ folder-7/review-JS-1-3.js
@@ -21,6 +21,29 @@ let longNameThatStartsWithA = findLongNameThatStartsWithA(names);
 
 console.log(longNameThatStartsWithA);
 
+//syntax of find method
+// let result = arr.find(function (item, index, array) {
+//   // if true is returned, item is returned and iteration is stopped
+//   // for falsy scenario returns undefined
+// });
+
+//example
+
+let users = [
+  { id: 1, name: "Anna" },
+  { id: 2, name: "Nina" },
+  { id: 3, name: "Mamad" },
+];
+
+let user = users.find((person) => person.id === 1);
+console.log(user.name); //Anna
+
+//The arr.findIndex method is essentially the same, but it returns the index where the element was found instead of the element itself and -1 is returned when nothing is found.
+const array1 = [5, 12, 8, 130, 44];
+
+const isLargNumber = (number) => number > 13;
+console.log(array1.findIndex(isLargNumber)); // returns index (3)
+
 /*
   You are given a program that logs pairings between mentors and students
   It fails because the array `pairsById` can contain null values
