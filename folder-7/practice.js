0a187fee240447f03c720b776f9c63d1db037bcc
Anahid <anahidhemmat2130@gmail.com>
2022-01-17 12:29:10 +0400

    loops

--- folder-7/practice.js
+++ folder-7/practice.js
@@ -108,3 +108,16 @@ function addUpTo3(n) {
   return n + addUpTo3(n - 1);
 }
 console.log(addUpTo3(3)); //6
+
+//Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself.
+//This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+function addIndexes(arr) {
+  let indexes = [];
+  for (let i = 0; i < arr.length; i++) {
+    indexes.push(i);
+  }
+  return indexes;
+}
+
+console.log(addIndexes([0, 0, 0, 0, 0])); //[ 0, 1, 2, 3, 4 ]
