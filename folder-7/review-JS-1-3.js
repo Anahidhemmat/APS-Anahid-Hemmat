f22dab1159cfec64317cd538fb3ef1dd57a2f33c
Anahid <anahidhemmat2130@gmail.com>
2022-01-17 21:02:36 +0400

    review

--- folder-7/review-JS-1-3.js
+++ folder-7/review-JS-1-3.js
@@ -239,3 +239,17 @@ function isInUK(country) {
 console.log(isInUK("France"));
 console.log(isInUK("Republic of Ireland"));
 console.log(isInUK("England"));
+
+/* 
+  You are given an statement
+
+  You should remove the word "and dogs" by using substring 
+*/
+
+let statement = "I like programming and dogs";
+
+statement = statement.substring(0, 18);
+
+console.log(statement);
+
+//substring() is basiclly a copy of slice
