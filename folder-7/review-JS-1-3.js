dce7d92eff8b467b9d160a07e4ffdaccebea0ccb
Anahid <anahidhemmat2130@gmail.com>
2022-01-17 20:01:32 +0400

    review

--- folder-7/review-JS-1-3.js
+++ folder-7/review-JS-1-3.js
@@ -185,3 +185,26 @@ let lastFive = everyone.slice(-5); // complete this statement
 
 console.log(firstFive);
 console.log(lastFive);
+
+/*
+  Array methods - .join()
+  -------------------------
+  Complete the capitalise function 
+  It should return a string with the first letter in uppercase
+  For example, capitailise("hello") should return "Hello"
+  Tip: use the string method .split() and the array method .join()
+*/
+
+function capitalise(str) {
+  let newArr = str.split("");
+  newArr[0] = newArr[0].toUpperCase();
+  return newArr.join("");
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+let sampleName = "benyamin";
+
+console.log(capitalise(sampleName));
+console.log(capitalise("hello"));
