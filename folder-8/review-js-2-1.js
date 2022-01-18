4865cc135a07807e6e335caaf87826b03847bf91
Anahid <anahidhemmat2130@gmail.com>
2022-01-18 22:13:43 +0400

    objects

--- folder-8/review-js-2-1.js
+++ folder-8/review-js-2-1.js
@@ -9,3 +9,67 @@ const myLaptop = {
   isNew: false,
   features: ["apps", "wide screen", "internet access"],
 };
+
+//Quiz 2
+const benyamin = {
+  name: "benyamin",
+  height: "176cm",
+  color: "black",
+  eyeColor: "abi they does have dog",
+};
+
+const tree = {
+  leafColor: "green",
+  age: 50,
+  isRed: false,
+  fruits: ["apple", "orange", "kiwi"],
+};
+//We can access properties with . notation and [ ] notation
+
+console.log(myLaptop.features[1]);
+
+let laptop = {
+  brand: "Lenovo",
+  screenSize: 13,
+  isTouchscreen: true,
+  colors: ["spaceGray", "black", "white"],
+  spec: {
+    owner: "vahid",
+    age: 24,
+    height: undefined,
+  },
+};
+
+console.log(laptop.spec.owner); //vahid
+console.log(laptop.colors[1][0]); //b
+console.log(laptop["brand"]); //lenovo
+console.log(laptop["colors"][2]); //white
+
+let keys = "name";
+
+// dynamic properties
+const myObj = {
+  age: 22,
+  height: 150,
+  [keys]: "mahdi",
+  //name: "mehdi",
+};
+console.log(myObj[keys]); //mehdi
+console.log(myObj.name); //mehdi
+console.log(myObj["name"]); //mehdi
+
+//functions in objects
+const anna = "Kiana";
+const jasem = {
+  isLoggedIn() {
+    return "User is logged in"; //regular func
+  },
+  isLoggedOut: () => {
+    return "User is logged out"; // arrow func
+  },
+  anna,
+};
+
+console.log(jasem.isLoggedIn());
+console.log(jasem.isLoggedOut());
+console.log(jasem.anna);
