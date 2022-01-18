4de1813c0bfb72576d6fcfd343a27ba02bac4ef1
Anahid <anahidhemmat2130@gmail.com>
2022-01-18 12:06:41 +0400

    map method

--- folder-8/practice.js
+++ folder-8/practice.js
@@ -60,3 +60,19 @@ let users2 = [john, pete, mary];
 
 let names = users2.map((person) => person.name);
 console.log(names); //[ 'John', 'Pete', 'Mary' ]
+
+//You have an array of user objects, each one has name, surname and id.
+//Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+let john2 = { name: "John", surname: "Smith", id: 1 };
+let pete2 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary2 = { name: "Mary", surname: "Key", id: 3 };
+
+let users3 = [john2, pete2, mary2];
+
+let usersMapped = users3.map((person) => ({
+  fullName: `${person.name} ${person.surname}`,
+  id: user.id,
+}));
+console.log(usersMapped);
+console.log(usersMapped[0].fullName);
