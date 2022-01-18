ad5811f30fa3067a9c5aadccf59feb6978050571
Anahid <anahidhemmat2130@gmail.com>
2022-01-18 12:53:06 +0400

    basics and variables

---
+++ folder-8/review-js-1-1.js
@@ -0,0 +1,27 @@
+//Basics
+//variables
+let age = 25;
+let year = 2019;
+
+console.log(age, year); //25 2019
+
+//we can change the value
+
+age = 30;
+console.log(age); //30
+
+//if we use const keyword we can't change the value
+
+const point = 100;
+//point = 50 => we will get an error
+
+//old way to creat variables
+var score = 75;
+console.log(score);
+
+//we write variable names in camel case we can't use spaces
+//variables can't start with numbers
+//we can't use reserved keywords for variables
+
+//we use // for single line comments
+//we use /* */ for multi-line comments
