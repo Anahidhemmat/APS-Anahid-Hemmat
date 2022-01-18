430c3d0a908241394baf513bb655ead93d1f543d
Anahid <anahidhemmat2130@gmail.com>
2022-01-18 21:03:28 +0400

    quiz

---
+++ folder-8/review-js-2-1.js
@@ -0,0 +1,11 @@
+//Objects
+//Quiz 1
+// Describe your own laptop as a JavaScript object. Try to think of as many properties as you can!
+
+const myLaptop = {
+  model: "MacBook",
+  color: "silver",
+  year: 2015,
+  isNew: false,
+  features: ["apps", "wide screen", "internet access"],
+};
