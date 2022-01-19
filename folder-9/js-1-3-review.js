fc7c5a805a8ef05d7fd7e711bc0f0c98bf306dd0
Anahid <anahidhemmat2130@gmail.com>
2022-01-19 12:44:54 +0400

    loops

---
+++ folder-9/js-1-3-review.js
@@ -0,0 +1,83 @@
+//For loop
+
+for (let i = 1; i <= 6; i++) {
+  console.log(i + "-in shabi k mign shab nist");
+}
+
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log(i);
+}
+
+for (let i = 50; i >= 0; i -= 10) {
+  console.log(i);
+}
+
+for (let i = 25; i >= 0; i -= 5) {
+  console.log(i);
+}
+
+//Quiz 3
+
+const students = ["Sirvan", "Pourya", "Alireza", "saba", "Khorzoor"];
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].toUpperCase());
+}
+
+//nested loop
+const airplane = [
+  ["niloofar", "kosar", "Fatemeh"],
+  ["ali", "mohsen", "Shirin"],
+  ["atena", "mahdi", "benyamin"],
+  ["bani", "mohammad", "sina"],
+  ["Amir", "Beti", "Pourya"],
+];
+
+for (let i = 0; i < airplane.length; i++) {
+  console.log(`Row - ${i + 1}`);
+  for (let j = 0; j < airplane[i].length; j++) {
+    console.log(`${airplane[i][j]}`);
+  }
+}
+
+//while loop
+
+const randomStuff = ["apple", 2, 4, null, true, "ana", ["cat", "ball"], "last"];
+
+// let x = randomStuff.length - 1;
+
+// while (x >= 0) {
+//   console.log(randomStuff[x]);
+//   x--;
+// }
+
+// let x2 = 0;
+// while (x2 < randomStuff.length) {
+//   console.log(randomStuff[x2]);
+//   x++;
+// };
+
+//break
+
+for (let i = 100; i >= 0; i--) {
+  console.log(i);
+  if (i === 50) {
+    break;
+  }
+}
+//Quiz 4
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let num of numbers) {
+  console.log(num * num);
+}
