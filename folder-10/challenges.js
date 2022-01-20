2414a9a31d52bf6be77340b48f0db759de5e920a
Anahid <anahidhemmat2130@gmail.com>
2022-01-20 20:05:05 +0400

    challenge

--- folder-10/challenges.js
+++ folder-10/challenges.js
@@ -24,3 +24,23 @@ function isEmpty(obj) {
 }
 
 console.log(isEmpty(schudle));
+
+//Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+//If salaries is empty, then the result must be 0.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+function salarySum(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  //isObjectEmpty
+  return Object.keys(obj).length === 0 ? 0 : sum;
+}
+
+console.log(salarySum(salaries)); //390
