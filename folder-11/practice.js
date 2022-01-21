0cfcabdcd4bad45cd79524a6ca4b3dfce0ea4f24
Anahid <anahidhemmat2130@gmail.com>
2022-01-21 15:32:08 +0400

    array method filter

--- folder-11/practice.js
+++ folder-11/practice.js
@@ -129,3 +129,14 @@ let camelize = (str) => {
 };
 
 console.log(camelize(str1));
+
+//Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+//The function should not modify the array. It should return the new array.
+
+let numbers2 = [5, 3, 8, 1];
+
+let filterRange = (arr, a, b) => {
+  return arr.filter((num) => num >= a && num <= b);
+};
+
+console.log(filterRange(numbers2, 1, 4)); //[3,1]
