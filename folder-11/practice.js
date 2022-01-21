d328dc7cb9f4da1bbc42ccb22d4e6e46014a7ad4
Anahid <anahidhemmat2130@gmail.com>
2022-01-21 11:30:05 +0400

    creating a calculator

--- folder-11/practice.js
+++ folder-11/practice.js
@@ -23,3 +23,24 @@ function makeUser2() {
 let user2 = makeUser2();
 
 console.log(user2.ref().name); //John
+
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+
+let calculator = {
+  read(num1, num2) {
+    return (calculator.num1 = num1), (calculator.num2 = num2);
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
+};
+calculator.read(3, 2);
+console.log(calculator.sum());
+console.log(calculator.mul());
