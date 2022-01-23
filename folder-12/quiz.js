9768602a556d44b0c2f12779c75b4b2692378077
Anahid <anahidhemmat2130@gmail.com>
2022-01-23 17:24:16 +0400

    quiz 1

---
+++ folder-12/quiz.js
@@ -0,0 +1,10 @@
+//Quiz 1
+
+// . Select the image element by it's id and save it to a variable called image
+// . Select the h1 by it's id and save it to a variable called heading
+
+const image = document.getElementById("unicorn");
+console.log(image);
+
+const heading = document.getElementById("mainheading");
+console.log(heading);
