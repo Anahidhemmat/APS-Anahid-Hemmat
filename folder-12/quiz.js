3728aaff449c30ba992e994949d6763d66225d48
Anahid <anahidhemmat2130@gmail.com>
2022-01-24 11:01:39 +0400

    selectors

--- folder-12/quiz.js
+++ folder-12/quiz.js
@@ -3,8 +3,17 @@
 // . Select the image element by it's id and save it to a variable called image
 // . Select the h1 by it's id and save it to a variable called heading
 
-const image = document.getElementById("unicorn");
-console.log(image);
+//getElementById
+const image = document.getElementById("unicorn"); //img element
 
-const heading = document.getElementById("mainheading");
-console.log(heading);
+const heading = document.getElementsByTagName("h1"); //h1 element
+
+//getElementsByTagName
+
+const allParagraphs = document.getElementsByTagName("p");
+console.log(allParagraphs); //html collections
+
+//getElementsByClassName
+
+const heading2 = document.getElementsByClassName("heading-2");
+console.log(heading2);
