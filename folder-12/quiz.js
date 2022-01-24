3a1d0c3cacade776c9c5dd48f44b5a7dd6d081a6
Anahid <anahidhemmat2130@gmail.com>
2022-01-24 11:49:05 +0400

    quiz 2

--- folder-12/quiz.js
+++ folder-12/quiz.js
@@ -4,16 +4,42 @@
 // . Select the h1 by it's id and save it to a variable called heading
 
 //getElementById
-const image = document.getElementById("unicorn"); //img element
+// const image = document.getElementById("unicorn"); //img element
 
-const heading = document.getElementsByTagName("h1"); //h1 element
+// const heading = document.getElementsByTagName("h1"); //h1 element
 
-//getElementsByTagName
+// //getElementsByTagName
 
-const allParagraphs = document.getElementsByTagName("p");
-console.log(allParagraphs); //html collections
+// const allParagraphs = document.getElementsByTagName("p");
+// console.log(allParagraphs); //html collections
 
-//getElementsByClassName
+// //getElementsByClassName
 
-const heading2 = document.getElementsByClassName("heading-2");
-console.log(heading2);
+// const heading2 = document.getElementsByClassName("heading-2");
+// console.log(heading2);
+
+/*
+Quiz 2 
+=======
+Write JavaScript below that logs:
+    1. all the "p" element nodes of the document,
+    --> should log a list of nodes with a length of 6
+    2. the first div element node
+    --> should log the ".site-header" node
+    3. the element with id "jumbotron-text"
+    --> should log the "#jumbotron-text" node
+    4. all the "p" elements of contained inside  the .primary-content element node
+    --> should log a list of nodes with a length of 3
+*/
+
+const allPara = document.querySelectorAll("p");
+console.log(allPara);
+
+const firstDiv = document.querySelector("div");
+console.log(firstDiv);
+
+const elementWithId = document.querySelector("#jumbotron-text");
+console.log(firstDiv);
+
+const allParaInClass = document.querySelectorAll(".primary-content p");
+console.log(allParaInClass);
