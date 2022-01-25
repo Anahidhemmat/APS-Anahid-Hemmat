e45c4eea7dde321e857f4e663495f19adfcc9fee
Anahid <anahidhemmat2130@gmail.com>
2022-01-25 17:31:58 +0400

    Find the Difference in Age between Oldest and Youngest Family Members

--- folder-13/codeWars.js
+++ folder-13/codeWars.js
@@ -24,3 +24,20 @@ var summation = function (num) {
 };
 
 console.log(summation(8)); //36
+
+//Find the Difference in Age between Oldest and Youngest Family Members
+let array = [82, 15, 6, 38, 35];
+
+function differenceInAges(ages) {
+  let newArr = [];
+  let youngest = Math.min(...ages);
+  console.log(youngest);
+  let oldest = Math.max(...ages);
+  console.log(oldest);
+  let difference = oldest - youngest;
+  console.log(difference);
+
+  newArr.push(youngest, oldest, difference);
+  return newArr;
+}
+console.log(differenceInAges(array));
