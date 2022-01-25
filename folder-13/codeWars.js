9968b3411741bde0b33b8b047f5db0cbfa672170
Anahid <anahidhemmat2130@gmail.com>
2022-01-25 17:22:42 +0400

    Summation

--- folder-13/codeWars.js
+++ folder-13/codeWars.js
@@ -1,5 +1,7 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+//Removing Elements
+
 function removeEveryOther(arr) {
   //your code here
   return arr.filter((element, index) => {
@@ -9,3 +11,16 @@ function removeEveryOther(arr) {
   });
 }
 console.log(removeEveryOther(numbers));
+
+//Grasshopper - Summation
+
+var summation = function (num) {
+  // Code here
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+console.log(summation(8)); //36
