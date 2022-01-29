798aaa7b4e6da8f5c1271434f32b712337febd53
Anahid <anahidhemmat2130@gmail.com>
2022-01-29 16:56:21 +0400

    bubbling & capturing

--- JS2-2.js/eventListeners.js
+++ JS2-2.js/eventListeners.js
@@ -3,12 +3,48 @@ const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
 //basics
-grandparent.addEventListener("click", function (e) {
-  console.log(e);
+// grandparent.addEventListener("click", function (e) {
+//   console.log(e);
+// });
+// grandparent.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
+// grandparent.addEventListener("click", function (e) {
+//   console.log("Grandparent 1");
+// });
+
+//event bubbling
+
+// grandparent.addEventListener("click", function (e) {
+//   console.log("grandparent");
+// });
+// parent.addEventListener("click", function (e) {
+//   console.log("parent");
+// });
+// child.addEventListener("click", function (e) {
+//   console.log("child");
+// });
+
+// document.addEventListener("click", function () {
+//   console.log("document");
+// });
+
+//event capturing
+
+grandparent.addEventListener(
+  "click",
+  function (e) {
+    console.log("grandparent");
+  },
+  { capture: true }
+);
+parent.addEventListener("click", function (e) {
+  console.log("parent");
 });
-grandparent.addEventListener("click", function (e) {
-  console.log(e.target);
+child.addEventListener("click", function (e) {
+  console.log("child");
 });
-grandparent.addEventListener("click", function (e) {
-  console.log("Grandparent 1");
+
+document.addEventListener("click", function () {
+  console.log("document");
 });
