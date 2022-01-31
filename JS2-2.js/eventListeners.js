f81ec8dbcb62265e19f981b08ee61b35cd2d8c60
Anahid <anahidhemmat2130@gmail.com>
2022-01-31 18:07:26 +0400

    event listeners

--- JS2-2.js/eventListeners.js
+++ JS2-2.js/eventListeners.js
@@ -2,6 +2,8 @@ const grandparent = document.querySelector(".grandparent");
 const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
+//options
+
 //basics
 // grandparent.addEventListener("click", function (e) {
 //   console.log(e);
@@ -31,20 +33,72 @@ const child = document.querySelector(".child");
 
 //event capturing
 
-grandparent.addEventListener(
-  "click",
-  function (e) {
-    console.log("grandparent");
-  },
-  { capture: true }
-);
-parent.addEventListener("click", function (e) {
-  console.log("parent");
-});
-child.addEventListener("click", function (e) {
-  console.log("child");
-});
+// grandparent.addEventListener(
+//   "click",
+//   function (e) {
+//     console.log("grandparent");
+//     e.stopPropagation();
+//   },
+//   { capture: true }
+// );
+// parent.addEventListener("click", function (e) {
+//   console.log("parent");
+// });
+// child.addEventListener("click", function (e) {
+//   console.log("child");
+// });
 
-document.addEventListener("click", function () {
-  console.log("document");
+// document.addEventListener("click", function () {
+//   console.log("document");
+// } , {once: true});
+
+//removeEventListener
+
+// grandparent.addEventListener("click", function (e) {
+//   console.log("grandparent");
+// });
+
+// parent.addEventListener("click", printHi);
+// setTimeout(() => {
+//   parent.removeEventListener("click", printHi);
+// }, 2000);
+
+// child.addEventListener("click", function (e) {
+//   console.log("child");
+// });
+
+// function printHi() {
+//   console.log("Hi");
+// }
+
+//event deligation
+
+const divs = document.querySelectorAll("div");
+
+// divs.forEach((div) => {
+//   div.addEventListener("click", function () {
+//     console.log("Hi");
+//   });
+// });
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.matches("div")) {
+//     console.log("hi");
+//   }
+// });
+
+//best way
+addGlobalEventListener("click", "div", (e) => {
+  console.log("hi");
 });
+function addGlobalEventListener(type, selector, callback) {
+  document.addEventListener(type, (e) => {
+    if (e.target.matches(selector)) callback(e);
+  });
+}
+
+const newDiv = document.createElement("div");
+newDiv.style.width = "200px";
+newDiv.style.height = "200px";
+newDiv.style.backgroundColor = "purple";
+document.body.append(newDiv);
