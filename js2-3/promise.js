2d10add7d71aea2c3fca26c2d4f79e5f5390e79a
Anahid <anahidhemmat2130@gmail.com>
2022-02-01 22:51:48 +0400

    promise.all

--- js2-3/promise.js
+++ js2-3/promise.js
@@ -26,3 +26,20 @@
 //   .catch((err) => {
 //     console.log(err);
 //   });
+
+//promise.all
+const yt = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("getting stuff from youtube");
+    resolve({ videos: [1, 2, 3, 4, 5] });
+  }, 2000);
+});
+
+const fb = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("getting stuff from facebook");
+    resolve({ user: "Name" });
+  }, 3000);
+});
+
+Promise.all([yt, fb]).then((result) => console.log(result));
