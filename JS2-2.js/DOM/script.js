514ff1f8a7210a9347c45f6416c4dbb5796acb9d
Anahid <anahidhemmat2130@gmail.com>
2022-02-08 17:57:24 +0400

    properties

--- JS2-2.js/DOM/script.js
+++ JS2-2.js/DOM/script.js
@@ -1,21 +1,41 @@
 const para = document.querySelector("p");
-console.log(para);
+//console.log(para);
 
 const div = document.querySelector("div.error");
 
-console.log(div);
+//console.log(div);
 
 const heading = document.querySelector("body > h1"); //copy selector from inspect => elements
 
-console.log(heading);
+//console.log(heading);
 
 const paras = document.querySelectorAll("p"); //Nodelist
 const errors = document.querySelectorAll(".error"); //Nodelist
 
 paras.forEach((para) => {
-  console.log(para);
+  //console.log(para);
 });
 
-console.log(paras[2]);
+//console.log(paras[2]);
 
-console.log(errors);
+//console.log(errors);
+
+//properties
+
+para.innerText += " Welcome to my page";
+
+div.innerText = "This is a div with an error class";
+
+paras.forEach((para) => {
+  para.innerText += " new text";
+});
+
+const content = document.querySelector(".content");
+
+content.innerHTML += "<h2>This is a new h2</h2>";
+
+const people = ["Mario", "Anna", "Kiana"];
+
+people.forEach((person) => {
+  content.innerHTML += `<p>${person}</p>`;
+});
