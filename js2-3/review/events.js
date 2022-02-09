42d185ae9fc360ab9f90eb470e4843e54d5d17cc
Anahid <anahidhemmat2130@gmail.com>
2022-02-09 19:57:17 +0400

    Bom

--- js2-3/review/events.js
+++ js2-3/review/events.js
@@ -13,17 +13,61 @@ const btn3 = btn2.nextElementSibling;
 //   alert("Ana");
 // };
 
-btn1.onclick = click;
+// btn1.onclick = click;
 
-function click() {
-  alert("mahya");
-  alert("rasa");
-}
+// function click() {
+//   alert("mahya");
+//   alert("rasa");
+// }
 
-btn1.addEventListener("click", function () {
-  alert("deniz");
+// btn1.addEventListener("click", function () {
+//   alert("deniz");
+// });
+// //won't be overwriten
+// btn2.addEventListener("click", function () {
+//   alert("deniz");
+// });
+
+//BOM => browser object model
+
+//resize
+const h1width = document.createElement("h1");
+const h1height = document.createElement("h1");
+document.body.append(h1width, h1height);
+
+btn3.addEventListener("click", () => {
+  h1width.textContent = window.innerWidth;
+  h1height.textContent = window.innerHeight;
 });
-//won't be overwriten
-btn2.addEventListener("click", function () {
-  alert("deniz");
+window.addEventListener("resize", (e) => {
+  //resize => window
+  console.log(e);
+  h1width.textContent = window.innerWidth;
+  h1height.textContent = window.innerHeight;
 });
+
+//also window possible
+document.body.addEventListener("keydown", (e) => {
+  console.log(e); //target => body
+});
+
+console.log(document); //elements
+console.dir(document); //objects
+
+const randomColor = () => {
+  const red = parseInt(Math.random() * 256);
+  const green = parseInt(Math.random() * 256);
+  const blue = parseInt(Math.random() * 256);
+
+  return `rgb(${red},${green},${blue})`;
+};
+btn3.addEventListener("click", () => {
+  //document.body.style.backgroundColor = randomColor();
+  document.body.style.backgroundColor = `rgb(${random()},${random()},${random()})`;
+});
+
+//another way
+
+const random = () => {
+  return parseInt(Math.random() * 256);
+};
