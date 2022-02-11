355394fb8b52071db18dede50d4d9eb4c33d90fb
Anahid <anahidhemmat2130@gmail.com>
2022-02-11 17:43:11 +0400

    promise

--- js2-3/promise.js
+++ js2-3/promise.js
@@ -47,63 +47,88 @@
 //promise
 
 //resolve
-let promise = new Promise(function (resolve, reject) {
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve("done");
+//   }, 1000);
+// });
+
+// //reject
+
+// let promise2 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     reject(new Error("Whooops"));
+//   }, 1000);
+// });
+
+// //Immediately calling resolve/reject
+
+// let promise3 = new Promise(function (resolve, reject) {
+//   resolve("done");
+// });
+
+// //.then()
+
+// promise.then(
+//   function (result) {},
+//   function (error) {}
+// );
+
+// promise.then(
+//   (result) => console.log(result),
+//   (error) => console.log(error)
+// );
+
+// //.catch()
+
+// promise2.catch(error);
+
+// //or
+
+// promise2.then(null, (error) => console.log(error));
+
+// //example
+
+// const loadScript = (src) => {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     script.src = src;
+
+//     script.onload = () => resolve(script);
+//     script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+//     document.head.append(script);
+//   });
+// };
+
+// let promise4 = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js");
+
+// promise4.then(
+//   (script) => console.log("here is your src  =>", script),
+//   (error) => console.log(error.massage)
+// );
+
+// let promise5 = new Promise(function (resolve, reject) {
+//   resolve(1);
+
+//   setTimeout(() => resolve(2), 1000);
+// });
+
+// promise5.then((data) => console.log(data));
+
+//excersise
+
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("done");
-  }, 1000);
+    console.log("We got the data");
+    resolve({ user: "ed" });
+  }, 2000);
 });
 
-//reject
-
-let promise2 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject(new Error("Whooops"));
-  }, 1000);
-});
-
-//Immediately calling resolve/reject
-
-let promise3 = new Promise(function (resolve, reject) {
-  resolve("done");
-});
-
-//.then()
-
-promise.then(
-  function (result) {},
-  function (error) {}
-);
-
-promise.then(
-  (result) => console.log(result),
-  (error) => console.log(error)
-);
-
-//.catch()
-
-promise2.catch(error);
-
-//or
-
-promise2.then(null, (error) => console.log(error));
-
-//example
-
-const loadScript = (src) => {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement("script");
-    script.src = src;
-
-    script.onload = () => resolve(script);
-    script.onerror = () => reject(new Error(`Script load error for ${src}`));
-
-    document.head.append(script);
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-};
-
-let promise4 = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js");
-
-promise4.then(
-  (script) => console.log("here is your src  =>", script),
-  (error) => console.log(error.massage)
-);
