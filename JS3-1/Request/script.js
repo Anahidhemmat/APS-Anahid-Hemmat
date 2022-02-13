53739bcc65184aff3eed9da1343f0c32c89b9e69
Anahid <anahidhemmat2130@gmail.com>
2022-02-13 21:29:14 +0400

    callback

--- JS3-1/Request/script.js
+++ JS3-1/Request/script.js
@@ -1,4 +1,4 @@
-const url = "https://jsonplaceholder.typicode.com/todos/ ";
+// const url = "https://jsonplaceholder.typicode.com/todos/ ";
 
 // const request = new XMLHttpRequest();
 
@@ -34,20 +34,70 @@ const url = "https://jsonplaceholder.typicode.com/todos/ ";
 //   }
 // };
 
-const request = new XMLHttpRequest();
+// const request = new XMLHttpRequest();
 
-request.open("GET", url); //(Method, File or url we want to get data from)
+// request.open("GET", url); //(Method, File or url we want to get data from)
 
-request.send(); //sending request
+// request.send(); //sending request
 
-request.onload = function () {
-  if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    //console.log(data);
-    for (let d of data) {
-      console.log(d);
+// request.onload = function () {
+//   if (request.status === 200) {
+//     const data = JSON.parse(request.responseText);
+//     //console.log(data);
+//     for (let d of data) {
+//       console.log(d);
+//     }
+//   } else {
+//     console.log("not found");
+//   }
+// };
+
+//callback
+
+const getTodos = function (url, callback) {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", url); //(Method, File or url we want to get data from)
+
+  request.send(); //sending request
+
+  request.onload = function () {
+    if (request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      //console.log(data);
+      // for (let d of data) {
+      //   console.log(d);
+      // }
+      callback(undefined, data);
+    } else {
+      // console.log("not found");
+      callback("not found", undefined);
     }
-  } else {
-    console.log("not found");
-  }
+  };
 };
+
+const url = "https://jsonplaceholder.typicode.com/todos/1 ";
+const url2 = "https://jsonplaceholder.typicode.com/todos/2 ";
+const url3 = "https://jsonplaceholder.typicode.com/todos/3 ";
+
+getTodos(url, function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    getTodos(url2, function (error, data) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(data);
+        getTodos(url3, function (error, data) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(data);
+          }
+        });
+      }
+    });
+  }
+});
