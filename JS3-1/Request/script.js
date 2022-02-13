0ea22122b57173212c694c3d008569efe7e5778b
Anahid <anahidhemmat2130@gmail.com>
2022-02-13 20:56:41 +0400

    request url

--- JS3-1/Request/script.js
+++ JS3-1/Request/script.js
@@ -1,4 +1,4 @@
-// const url = "https://jsonplaceholder.typicode.com/todos/1";
+const url = "https://jsonplaceholder.typicode.com/todos/ ";
 
 // const request = new XMLHttpRequest();
 
@@ -20,15 +20,33 @@
 //   }
 // };
 
+// const request = new XMLHttpRequest();
+
+// request.open("GET", "data.txt"); //(Method, File or url we want to get data from)
+
+// request.send(); //sending request
+
+// request.onload = function () {
+//   if (request.status === 200) {
+//     console.log(request);
+//   } else {
+//     console.log("not found");
+//   }
+// };
+
 const request = new XMLHttpRequest();
 
-request.open("GET", "data.txt"); //(Method, File or url we want to get data from)
+request.open("GET", url); //(Method, File or url we want to get data from)
 
 request.send(); //sending request
 
 request.onload = function () {
   if (request.status === 200) {
-    console.log(request);
+    const data = JSON.parse(request.responseText);
+    //console.log(data);
+    for (let d of data) {
+      console.log(d);
+    }
   } else {
     console.log("not found");
   }
