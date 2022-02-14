7f6ed6280b4c54e1aa46ab327e814d140d8ce2fa
Anahid <anahidhemmat2130@gmail.com>
2022-02-14 19:03:19 +0400

    promise

--- JS3-1/Request/script.js
+++ JS3-1/Request/script.js
@@ -80,24 +80,64 @@ const url = "https://jsonplaceholder.typicode.com/todos/1 ";
 const url2 = "https://jsonplaceholder.typicode.com/todos/2 ";
 const url3 = "https://jsonplaceholder.typicode.com/todos/3 ";
 
-getTodos(url, function (error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-    getTodos(url2, function (error, data) {
-      if (error) {
-        console.log(error);
+// getTodos(url, function (error, data) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//     getTodos(url2, function (error, data) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log(data);
+//         getTodos(url3, function (error, data) {
+//           if (error) {
+//             console.log(error);
+//           } else {
+//             console.log(data);
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
+
+const getTodos = function (url, callback) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.open("GET", url); //(Method, File or url we want to get data from)
+
+    request.send(); //sending request
+
+    request.onload = function () {
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        //console.log(data);
+        // for (let d of data) {
+        //   console.log(d);
+        // }
+        resolve(undefined, data);
       } else {
-        console.log(data);
-        getTodos(url3, function (error, data) {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(data);
-          }
-        });
+        // console.log("not found");
+        reject("not found", undefined);
       }
-    });
-  }
-});
+    };
+  });
+};
+
+getTodos(url)
+  .then((data) => {
+    console.log(data);
+    return getTodos(url2);
+  })
+  .then((data) => {
+    console.log(data);
+    return getTodos(url3);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
