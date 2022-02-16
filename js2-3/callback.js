8987cc016b8f7d68dede905db6731c24a0394a9b
Anahid <anahidhemmat2130@gmail.com>
2022-02-16 13:51:22 +0400

    sync

--- js2-3/callback.js
+++ js2-3/callback.js
@@ -25,25 +25,50 @@
 
 //another example
 
-function loadScript(src, callback) {
-  let script = document.createElement("script");
-  script.src = src;
-  script.onload = () => callback(null, script);
-  script.onerror = () => callback(new Error(`script load error for ${src}`));
-  document.head.append(script);
-}
+// function loadScript(src, callback) {
+//   let script = document.createElement("script");
+//   script.src = src;
+//   script.onload = () => callback(null, script);
+//   script.onerror = () => callback(new Error(`script load error for ${src}`));
+//   document.head.append(script);
+// }
 
-// loadScript("/my/script.js", (script) => {
-//   console.log(`The script ${script.src} loaded.`);
-//   loadScript("/my/script2.js", (script) => {
-//     console.log(`The second script is loaded`);
-//   });
+// // loadScript("/my/script.js", (script) => {
+// //   console.log(`The script ${script.src} loaded.`);
+// //   loadScript("/my/script2.js", (script) => {
+// //     console.log(`The second script is loaded`);
+// //   });
+// // });
+
+// loadScript("/my/script.js", (error, script) => {
+//   if (error) {
+//     console.log("Script didn't load");
+//   } else {
+//     console.log(`The script  ${script.src} loaded`);
+//   }
 // });
 
-loadScript("/my/script.js", (error, script) => {
-  if (error) {
-    console.log("Script didn't load");
-  } else {
-    console.log(`The script  ${script.src} loaded`);
-  }
-});
+//crash course
+
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post two", body: "This is post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPosts(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+getPosts();
+createPosts({ title: "Post three", body: "This is post three" });
