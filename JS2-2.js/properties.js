8a62c3e7068a231362262b45e3d954afb4d53f55
Anahid <anahidhemmat2130@gmail.com>
2022-02-16 15:09:51 +0400

    DOM properties

--- JS2-2.js/properties.js
+++ JS2-2.js/properties.js
@@ -17,18 +17,18 @@ const paragraph = firstBold.parentElement;
 // console.log(paragraph.children[3]);
 
 const squareImg = document.querySelector(".square");
-console.log(squareImg);
+//console.log(squareImg);
 
-console.log(squareImg.nextSibling); //text
-console.log(squareImg.nextElementSibling); //img
+// console.log(squareImg.nextSibling); //text
+// console.log(squareImg.nextElementSibling); //img
 
-console.log(squareImg.previousSibling); //text
-console.log(squareImg.previousElementSibling); //p
+// console.log(squareImg.previousSibling); //text
+// console.log(squareImg.previousElementSibling); //p
 
 //Create Element
 
 const newImg = document.createElement("img");
-console.dir(newImg);
+//console.dir(newImg);
 newImg.setAttribute(
   "src",
   "https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80"
@@ -46,7 +46,7 @@ document.body.appendChild(h3);
 //Append
 
 const p = document.querySelector("p");
-console.log(p);
+//console.log(p);
 
 p.append(
   "I am new text, want to write a long text alaki",
@@ -55,7 +55,7 @@ p.append(
 
 const newB = document.createElement("b");
 newB.append("harchi ....");
-console.log(newB);
+//console.log(newB);
 //p.append(newB);
 //p.prepend(newB);
 
@@ -72,4 +72,21 @@ const h1 = document.querySelector("h1");
 //h1.insertAdjacentElement("afterbegin", h2);
 
 //h1.insertAdjacentElement("beforeend", h2);
-h1.insertAdjacentElement("afterend", h2);
+//h1.insertAdjacentElement("afterend", h2);
+
+//remove
+
+const firstLi = document.querySelector("li");
+console.log(firstLi);
+
+//firstLi.removeChild(firstLi); //error
+
+// const ul = firstLi.parentElement;
+// ul.removeChild(firstLi);
+
+// const b = document.querySelector("b");
+// b.parentElement.removeChild(b);
+
+//or
+const firstImg = document.querySelector("img");
+firstImg.remove();
