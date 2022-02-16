ad13294ea3b2dc250be0489dcb878a43b240d8dc
Anahid <anahidhemmat2130@gmail.com>
2022-02-16 18:16:59 +0400

    callback version

--- JS3-1/Net ninja async course/sandbox.js
+++ JS3-1/Net ninja async course/sandbox.js
@@ -1,22 +1,48 @@
 //HTTP REQUEST
 
-//Ceating Request Object
-const request = new XMLHttpRequest();
+// //Ceating Request Object
+// const request = new XMLHttpRequest();
 
-// XMLHttpRequest.readystate => one of XMLHttpRequest properties
-//readystatechange event
+// // XMLHttpRequest.readystate => one of XMLHttpRequest properties
+// //readystatechange event
 
-request.addEventListener("readystatechange", () => {
-  //   console.log(request, request.readyState);
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText); //responseText is a property that contains response data
-  } else if (request.readyState === 4) {
-    console.log("could not fetch the data");
+// request.addEventListener("readystatechange", () => {
+//   //   console.log(request, request.readyState);
+//   if (request.readyState === 4 && request.status === 200) {
+//     console.log(request.responseText); //responseText is a property that contains response data
+//   } else if (request.readyState === 4) {
+//     console.log("could not fetch the data");
+//   }
+// });
+
+// //open(Method, End Point);
+// request.open("GET", "https://jsonplaceholder.typicode.com/todos/ ");
+
+// //send();
+// request.send();
+
+//CALLBACK VERSION =>async
+
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.statusText);
+    } else if (request.readyState === 4) {
+      callback("could not fetch the data", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/ ");
+  request.send();
+};
+
+getTodos((err, data) => {
+  console.log("callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
-
-//open(Method, End Point);
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/ ");
-
-//send();
-request.send();
