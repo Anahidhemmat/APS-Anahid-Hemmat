84f3de837dfe773e31b26ab368564d05a5db1e63
Anahid <anahidhemmat2130@gmail.com>
2022-02-16 15:11:46 +0400

    async & await

--- JS3-1/async.js
+++ JS3-1/async.js
@@ -1,9 +1,31 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 
-//async function
-setTimeout(() => {
-  console.log("some data");
-}, 3000);
-console.log(3);
-console.log(4);
+// //async function
+// setTimeout(() => {
+//   console.log("some data");
+// }, 3000);
+// console.log(3);
+// console.log(4);
+
+//async await
+
+const url = "https://jsonplaceholder.typicode.com/todos/2 ";
+
+const getTodos = async (url) => {
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Not found!!!");
+  }
+};
+
+getTodos(url)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
