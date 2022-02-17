f2d4902b194132dd253d12a0328f052cf49df30a
Anahid <anahidhemmat2130@gmail.com>
2022-02-17 14:19:08 +0400

    callback hell

--- JS3-1/Net ninja async course/sandbox.js
+++ JS3-1/Net ninja async course/sandbox.js
@@ -51,7 +51,35 @@
 
 //JSON
 
-const getTodos = (callback) => {
+// const getTodos = (callback) => {
+//   const request = new XMLHttpRequest();
+
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//       //JSON
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4) {
+//       callback("could not fetch the data", undefined);
+//     }
+//   });
+
+//   request.open("GET", "jsons/todos.json");
+//   request.send();
+// };
+
+// getTodos((err, data) => {
+//   console.log("callback fired");
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+//CALLBACK HELL
+
+const getTodos = (url, callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
@@ -64,15 +92,16 @@ const getTodos = (callback) => {
     }
   });
 
-  request.open("GET", "todos.json");
+  request.open("GET", url);
   request.send();
 };
 
-getTodos((err, data) => {
-  console.log("callback fired");
-  if (err) {
-    console.log(err);
-  } else {
+getTodos("jsons/todos.json", (err, data) => {
+  console.log(data);
+  getTodos("jsons/books.json", (err, data) => {
     console.log(data);
-  }
+    getTodos("jsons/users.json", (err, data) => {
+      console.log(data);
+    });
+  });
 });
