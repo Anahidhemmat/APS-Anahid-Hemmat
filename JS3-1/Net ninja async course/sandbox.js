85bcaba653271bea4f584a5ae808d70681c9e41f
Anahid <anahidhemmat2130@gmail.com>
2022-02-17 13:14:20 +0400

    json data

--- JS3-1/Net ninja async course/sandbox.js
+++ JS3-1/Net ninja async course/sandbox.js
@@ -21,20 +21,50 @@
 // //send();
 // request.send();
 
-//CALLBACK VERSION =>async
+//CALLBACK VERSION => async
+
+// const getTodos = (callback) => {
+//   const request = new XMLHttpRequest();
+
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//       //JSON
+//       const data = JSON.parse(request.statusText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4) {
+//       callback("could not fetch the data", undefined);
+//     }
+//   });
+
+//   request.open("GET", "https://jsonplaceholder.typicode.com/todos/ ");
+//   request.send();
+// };
+
+// getTodos((err, data) => {
+//   console.log("callback fired");
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+//JSON
 
 const getTodos = (callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.statusText);
+      //JSON
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       callback("could not fetch the data", undefined);
     }
   });
 
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/ ");
+  request.open("GET", "todos.json");
   request.send();
 };
 
