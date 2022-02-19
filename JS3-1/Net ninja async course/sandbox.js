4f66bbe06e56ef57f74bd93d09ce4f7e22dde180
Anahid <anahidhemmat2130@gmail.com>
2022-02-19 15:54:23 +0400

    Promise version

--- JS3-1/Net ninja async course/sandbox.js
+++ JS3-1/Net ninja async course/sandbox.js
@@ -79,29 +79,76 @@
 
 //CALLBACK HELL
 
-const getTodos = (url, callback) => {
-  const request = new XMLHttpRequest();
+// const getTodos = (url, callback) => {
+//   const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      //JSON
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch the data", undefined);
-    }
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//       //JSON
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4) {
+//       callback("could not fetch the data", undefined);
+//     }
+//   });
+
+//   request.open("GET", url);
+//   request.send();
+// };
+
+// getTodos("jsons/todos.json", (err, data) => {
+//   console.log(data);
+//   getTodos("jsons/books.json", (err, data) => {
+//     console.log(data);
+//     getTodos("jsons/users.json", (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+//Promise Basics
+
+// const getData = () => {
+//   return new Promise((resolve, reject) => {
+//     //fetch something
+//     // resolve("some data");
+//     reject("some error");
+//   });
+// };
+
+// getData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//PROMISE VERSION => async
+
+const getTodos = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        //JSON
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("error getting data");
+      }
+    });
+
+    request.open("GET", url);
+    request.send();
   });
-
-  request.open("GET", url);
-  request.send();
 };
 
-getTodos("jsons/todos.json", (err, data) => {
-  console.log(data);
-  getTodos("jsons/books.json", (err, data) => {
-    console.log(data);
-    getTodos("jsons/users.json", (err, data) => {
-      console.log(data);
-    });
+getTodos("jsons/books.json")
+  .then((data) => {
+    console.log("Data recived:", data);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
