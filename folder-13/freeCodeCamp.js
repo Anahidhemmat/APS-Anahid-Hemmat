8b86a5adf1afe70ca61bd63c4a29347e1af28f95
Anahid <anahidhemmat2130@gmail.com>
2022-02-21 22:00:43 +0400

    Use class Syntax to Define a Constructor Function

--- folder-13/freeCodeCamp.js
+++ folder-13/freeCodeCamp.js
@@ -41,3 +41,15 @@ const createPerson = (name, age, gender) => {
   };
   // Only change code above this line
 };
+
+//Use class Syntax to Define a Constructor Function
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // Should display 'carrot'
