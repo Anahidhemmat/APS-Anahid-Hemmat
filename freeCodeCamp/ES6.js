f1dc4d3957ea9745c2a1950d4f492a10dc5cccc3
Anahid <anahidhemmat2130@gmail.com>
2022-02-22 22:02:49 +0400

    Title Case a Sentence

--- freeCodeCamp/ES6.js
+++ freeCodeCamp/ES6.js
@@ -185,3 +185,17 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+//Title Case a Sentence
+
+function titleCase(str) {
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] =
+      newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
+}
+
+titleCase("I'm a little tea pot");
