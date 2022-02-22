b4cf8fd65fb03be657cf3f9a0a67bff6c602c88d
Anahid <anahidhemmat2130@gmail.com>
2022-02-22 21:29:29 +0400

    Repeat a String

--- freeCodeCamp/ES6.js
+++ freeCodeCamp/ES6.js
@@ -171,3 +171,17 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding("Bastian", "n");
+
+//Repeat a String Repeat a String
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return "";
+  }
+  let newStr = "";
+  for (let i = 1; i <= num; i++) {
+    newStr += str;
+  }
+  return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
