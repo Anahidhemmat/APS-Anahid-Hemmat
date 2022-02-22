f4d99a8342c1a079b4f4f51ae5b4447a0da3d89b
Anahid <anahidhemmat2130@gmail.com>
2022-02-22 21:07:41 +0400

    Confirm the Ending

--- freeCodeCamp/ES6.js
+++ freeCodeCamp/ES6.js
@@ -145,3 +145,29 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(Math.max(...arr[i]));
+  }
+  return newArr;
+}
+
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
+
+// Confirm the Ending
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("Bastian", "n");
