6576466e73c376cf9caa24f96d59ab771d7b99b4
Anahid <anahidhemmat2130@gmail.com>
2022-02-28 16:45:21 +0400

    task

--- JS3-2/class codes/script.js
+++ JS3-2/class codes/script.js
@@ -61,3 +61,32 @@ admin.fullName = "Anahid Hemmat";
 console.log(user);
 console.log(admin.fullName); //Anahid Hemmat
 console.log(user.fullName); //John Smith
+
+//Task
+
+// Use __proto__ to assign prototypes in a way that any property lookup will
+// follow the path: pockets → bed → table → head. For instance, pockets.pen
+// should be 3 (found in table), and bed.glasses should be 1 (found in head).
+
+let head = {
+  glasses: 1,
+};
+
+let table = {
+  pen: 3,
+  __proto__: head,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table,
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed,
+};
+
+console.log(pockets.pen); //3
+console.log(bed.glasses); //1
