43a2fc746a7be62a7b1e4149077b038d43686ee3
Anahid <anahidhemmat2130@gmail.com>
2022-03-01 18:24:07 +0400

    Extend Constructors to Receive Arguments

--- JS3-2/freeCodeCamp OOP/script.js
+++ JS3-2/freeCodeCamp OOP/script.js
@@ -1,7 +1,19 @@
 //Define a Constructor Function
 
+// function Dog(name, color, numLegs) {
+//   this.name = "spot";
+//   this.color = "blue";
+//   this.numLegs = 4;
+// }
+
+// const hound = new Dog();
+
+//Extend Constructors to Receive Arguments
 function Dog(name, color, numLegs) {
-  this.name = "spot";
-  this.color = "blue";
+  this.name = name;
+  this.color = color;
   this.numLegs = 4;
 }
+
+const hound = new Dog("spot", "gray");
+console.log(hound);
