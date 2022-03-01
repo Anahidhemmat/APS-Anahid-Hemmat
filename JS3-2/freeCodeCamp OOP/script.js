f2e3e154f90b302d2f3cfc479704322a5f4e0a15
Anahid <anahidhemmat2130@gmail.com>
2022-03-01 18:47:15 +0400

    Understand Own Properties

--- JS3-2/freeCodeCamp OOP/script.js
+++ JS3-2/freeCodeCamp OOP/script.js
@@ -21,3 +21,15 @@ console.log(hound);
 //Verify an Object's Constructor with instanceof operator
 
 console.log(hound instanceof Dog); //true
+
+//Understand Own Properties => check direct properties
+
+let ownProps = [];
+
+for (let prop in hound) {
+  if (hound.hasOwnProperty(prop)) {
+    ownProps.push(prop);
+  }
+}
+
+console.log(ownProps);
